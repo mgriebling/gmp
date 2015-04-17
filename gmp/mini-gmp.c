df55c5fbd77c1337543ece1f9603a25a88deab10
Michael Griebling <michael.griebling@solinst.com>
2015-04-17 16:22:47 -0400

    More additions.

--- gmp/mini-gmp.c
+++ gmp/mini-gmp.c
@@ -4531,15 +4531,6 @@ mpf_add (mpf_ptr r, mpf_srcptr u, mpf_srcptr v)
 		vsize = prec - ediff;
 	}
 	
-#if 0
-	/* Locate the least significant non-zero limb in (the needed parts
-	 of) U and V, to simplify the code below.  */
-	while (up[0] == 0)
-		up++, usize--;
-	while (vp[0] == 0)
-		vp++, vsize--;
-#endif
-	
 	/* Allocate temp space for the result.  Allocate
 	 just vsize + ediff later???  */
 	tp = TMP_ALLOC_LIMBS (prec);
@@ -4602,6 +4593,244 @@ mpf_add (mpf_ptr r, mpf_srcptr u, mpf_srcptr v)
 	r->_mp_size = negate ? -rsize : rsize;
 	r->_mp_exp = uexp;
 	TMP_FREE;
+}
+
+void
+mpf_abs (mpf_ptr r, mpf_srcptr u)
+{
+	mp_size_t size;
+	
+	size = ABS (u->_mp_size);
+	if (r != u)
+	{
+		mp_size_t prec;
+		mp_ptr rp, up;
+		
+		prec = r->_mp_prec + 1;	/* lie not to lose precision in assignment */
+		rp = r->_mp_d;
+		up = u->_mp_d;
+		
+		if (size > prec)
+		{
+	  up += size - prec;
+	  size = prec;
+		}
+		
+		MPN_COPY (rp, up, size);
+		r->_mp_exp = u->_mp_exp;
+	}
+	r->_mp_size = size;
+}
+
+void
+mpf_add_ui (mpf_ptr sum, mpf_srcptr u, unsigned long int v)
+{
+	mp_srcptr up = u->_mp_d;
+	mp_ptr sump = sum->_mp_d;
+	mp_size_t usize, sumsize;
+	mp_size_t prec = sum->_mp_prec;
+	mp_exp_t uexp = u->_mp_exp;
+	
+	usize = u->_mp_size;
+	if (usize <= 0)
+	{
+		if (usize == 0)
+		{
+	  mpf_set_ui (sum, v);
+	  return;
+		}
+		else
+		{
+	  __mpf_struct u_negated;
+	  u_negated._mp_size = -usize;
+	  u_negated._mp_exp = u->_mp_exp;
+	  u_negated._mp_d = u->_mp_d;
+	  mpf_sub_ui (sum, &u_negated, v);
+	  sum->_mp_size = -(sum->_mp_size);
+	  return;
+		}
+	}
+	
+	if (v == 0)
+	{
+	sum_is_u:
+		if (u != sum)
+		{
+	  sumsize = MIN (usize, prec + 1);
+	  MPN_COPY (sum->_mp_d, up + usize - sumsize, sumsize);
+	  sum->_mp_size = sumsize;
+	  sum->_mp_exp = u->_mp_exp;
+		}
+		return;
+	}
+	
+	if (uexp > 0)
+	{
+		/* U >= 1.  */
+		if (uexp > prec)
+		{
+	  /* U >> V, V is not part of final result.  */
+	  goto sum_is_u;
+		}
+		else
+		{
+	  /* U's "limb point" is somewhere between the first limb
+	   and the PREC:th limb.
+	   Both U and V are part of the final result.  */
+	  if (uexp > usize)
+	  {
+		  /*   uuuuuu0000. */
+		  /* +          v. */
+		  /* We begin with moving U to the top of SUM, to handle
+		   samevar(U,SUM).  */
+		  MPN_COPY_DECR (sump + uexp - usize, up, usize);
+		  sump[0] = v;
+		  MPN_ZERO (sump + 1, uexp - usize - 1);
+#if 0 /* What is this??? */
+		  if (sum == u)
+			  MPN_COPY (sum->_mp_d, sump, uexp);
+#endif
+		  sum->_mp_size = uexp;
+		  sum->_mp_exp = uexp;
+	  }
+	  else
+	  {
+		  /*   uuuuuu.uuuu */
+		  /* +      v.     */
+		  mp_limb_t cy_limb;
+		  if (usize > prec)
+		  {
+			  /* Ignore excess limbs in U.  */
+			  up += usize - prec;
+			  usize -= usize - prec; /* Eq. usize = prec */
+		  }
+		  if (sump != up)
+			  MPN_COPY_INCR (sump, up, usize - uexp);
+		  cy_limb = mpn_add_1 (sump + usize - uexp, up + usize - uexp,
+							   uexp, (mp_limb_t) v);
+		  sump[usize] = cy_limb;
+		  sum->_mp_size = usize + cy_limb;
+		  sum->_mp_exp = uexp + cy_limb;
+	  }
+		}
+	}
+	else
+	{
+		/* U < 1, so V > U for sure.  */
+		/* v.         */
+		/*  .0000uuuu */
+		if ((-uexp) >= prec)
+		{
+	  sump[0] = v;
+	  sum->_mp_size = 1;
+	  sum->_mp_exp = 1;
+		}
+		else
+		{
+	  if (usize + (-uexp) + 1 > prec)
+	  {
+		  /* Ignore excess limbs in U.  */
+		  up += usize + (-uexp) + 1 - prec;
+		  usize -= usize + (-uexp) + 1 - prec;
+	  }
+	  if (sump != up)
+		  MPN_COPY_INCR (sump, up, usize);
+	  MPN_ZERO (sump + usize, -uexp);
+	  sump[usize + (-uexp)] = v;
+	  sum->_mp_size = usize + (-uexp) + 1;
+	  sum->_mp_exp = 1;
+		}
+	}
+}
+
+/* dir==1 for ceil, dir==-1 for floor
+ 
+ Notice the use of prec+1 ensures mpf_ceil and mpf_floor are equivalent to
+ mpf_set if u is already an integer.  */
+
+static void
+mpf_ceil_or_floor (mpf_ptr r, mpf_srcptr u, int dir)
+{
+	mp_ptr     rp, up, p;
+	mp_size_t  size, asize, prec;
+	mp_exp_t   exp;
+	
+	size = SIZ(u);
+	if (size == 0)
+	{
+	zero:
+		SIZ(r) = 0;
+		EXP(r) = 0;
+		return;
+	}
+	
+	rp = PTR(r);
+	exp = EXP(u);
+	if (exp <= 0)
+	{
+		/* u is only a fraction */
+		if ((size ^ dir) < 0)
+			goto zero;
+		rp[0] = 1;
+		EXP(r) = 1;
+		SIZ(r) = dir;
+		return;
+	}
+	EXP(r) = exp;
+	
+	up = PTR(u);
+	asize = ABS (size);
+	up += asize;
+	
+	/* skip fraction part of u */
+	asize = MIN (asize, exp);
+	
+	/* don't lose precision in the copy */
+	prec = PREC (r) + 1;
+	
+	/* skip excess over target precision */
+	asize = MIN (asize, prec);
+	
+	up -= asize;
+	
+	if ((size ^ dir) >= 0)
+	{
+		/* rounding direction matches sign, must increment if ignored part is
+		 non-zero */
+		for (p = PTR(u); p != up; p++)
+		{
+			if (*p != 0)
+			{
+				if (mpn_add_1 (rp, up, asize, CNST_LIMB(1)))
+				{
+					/* was all 0xFF..FFs, which have become zeros, giving just
+					 a carry */
+					rp[0] = 1;
+					asize = 1;
+					EXP(r)++;
+				}
+				SIZ(r) = (size >= 0 ? asize : -asize);
+				return;
+			}
+		}
+	}
+	
+	SIZ(r) = (size >= 0 ? asize : -asize);
+	if (rp != up)
+		MPN_COPY_INCR (rp, up, asize);
+}
+
+
+void
+mpf_ceil (mpf_ptr r, mpf_srcptr u)
+{
+	mpf_ceil_or_floor (r, u, 1);
+}
+
+void
+mpf_floor (mpf_ptr r, mpf_srcptr u)
+{
+	mpf_ceil_or_floor (r, u, -1);
 }
 
 
