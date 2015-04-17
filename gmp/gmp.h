dd282d5f35cb2fca8e153c1c2904bd3962a4f151
Michael Griebling <michael.griebling@solinst.com>
2015-04-17 08:11:05 -0400

    Initial Commit

---
+++ gmp/gmp.h
@@ -0,0 +1,19 @@
+//
+//  gmp.h
+//  gmp
+//
+//  Created by Michael Griebling on 17Apr2015.
+//  Copyright (c) 2015 Solinst Canada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for gmp.
+FOUNDATION_EXPORT double gmpVersionNumber;
+
+//! Project version string for gmp.
+FOUNDATION_EXPORT const unsigned char gmpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gmp/PublicHeader.h>
+
+
